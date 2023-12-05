dd0d1ec1dfb3461a193400eb6786c23f321f0314
Wesley Rodrigues <122097397+dev-ley@users.noreply.github.com>
2023-12-05 11:49:37 -0300

    quase la

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+//  Menu Responsivo 
+
 const MenuResponsivo = document.querySelector('.menu-responsivo')
 const Menu = document.querySelector('.navbar-mobile')
 
@@ -11,8 +14,18 @@ MenuResponsivo.addEventListener('click', function toggleElemento() {
     }
 )
 
+// Alert De Download
 const btdownloadcurriculo = document.querySelector('#curriculo')
 
 btdownloadcurriculo.addEventListener('click', function() {
     alert('Seu Download inicionou!');
+})
+
+// Dark & Light Mode 
+
+const chk = document.getElementById('chk')
+
+chk.addEventListener('change', () => {
+  document.body.classList.toggle('dark')
+
 })
